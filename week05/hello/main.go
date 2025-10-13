765463bf0aab570ac6054634dfd2c3ece876724a
Hyunseo Lee <hyunseoleessd@gmail.com>
2025-10-13 10:32:56 +0900

    loops/main.go v0.1  castting

---
+++ week05/hello/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Go developers"
+	fmt.Println("Git/Github for", name)
+
+}
---
+++ week07/loops/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var length float64 = 1.2
+	var width int = 2
+	fmt.Println("면적은 ", int(length)*width)
+	fmt.Println("면적은 ", int(length)*width)
+	fmt.Println("길이 > 너비?", length > float64(width))
+	fmt.Println("원본", reflect.TypeOf(float64(width)))
+}
