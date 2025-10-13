9a01e7b9b0acd5c162df7fd7c956f3076604b894
Hyunseo Lee <hyunseoleessd@gmail.com>
2025-10-13 11:37:32 +0900

    loops/main.go v0.1 if

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -3,19 +3,35 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
-	"time"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	var month time.Month = now.Month()
-	fmt.Println(month)
+	//shadowing
+	// fmt string = "inha"
+	//var int int = 7
+	//var k int = 11
+	//fmt.Println(int)
 
 	r := bufio.NewReader(os.Stdin)
 	i, err := r.ReadString('\n')
 	//fmt.Println(err)
-	log.Fatal(err) //repost the error and exit program
+
+	i = strings.TrimSpace(i)    // 개행 문자 제거
+	num, err := strconv.Atoi(i) // 문자열 → 정수 변환
+
+	if err != nil {
+		fmt.Println("숫자로 변환할 수 없습니다.")
+		return
+	}
+
+	if num >= 60 {
+		fmt.Println("pass")
+	} else {
+		fmt.Println("Fail")
+	}
+	//log.Fatal(err) //repost the error and exit program
 	fmt.Println(i)
 }
